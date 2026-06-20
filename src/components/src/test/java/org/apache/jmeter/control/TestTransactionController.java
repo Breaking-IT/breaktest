75f4c1f6654f62079026a448879c9ad8f8295431
jgaalen <jvangaalen@breakingit.nl>
2026-06-20 12:54:54 +0200

    Add transaction controller delay and pacing

--- src/components/src/test/java/org/apache/jmeter/control/TestTransactionController.java
+++ src/components/src/test/java/org/apache/jmeter/control/TestTransactionController.java
@@ -18,11 +18,21 @@
 package org.apache.jmeter.control;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
 
 import org.apache.jmeter.assertions.ResponseAssertion;
+import org.apache.jmeter.engine.util.CompoundVariable;
+import org.apache.jmeter.engine.util.ReplaceStringWithFunctions;
 import org.apache.jmeter.junit.JMeterTestCase;
 import org.apache.jmeter.sampler.DebugSampler;
 import org.apache.jmeter.test.samplers.CollectSamplesListener;
+import org.apache.jmeter.testelement.property.JMeterProperty;
+import org.apache.jmeter.testelement.property.StringProperty;
 import org.apache.jmeter.threads.JMeterContextService;
 import org.apache.jmeter.threads.JMeterThread;
 import org.apache.jmeter.threads.JMeterVariables;
@@ -34,6 +44,34 @@ import org.junit.jupiter.api.Test;
 
 
 public class TestTransactionController extends JMeterTestCase {
+
+    private static final Method COMPUTE_TRANSACTION_DELAY;
+
+    private static final Method COMPUTE_TRANSACTION_PACING_DELAY;
+
+    private static final Method RECORD_TRANSACTION_START;
+
+    private static final Field NEXT_PACING_START_TIME;
+
+    static {
+        try {
+            COMPUTE_TRANSACTION_DELAY = TransactionController.class.getDeclaredMethod("computeTransactionDelay");
+            COMPUTE_TRANSACTION_DELAY.setAccessible(true);
+            COMPUTE_TRANSACTION_PACING_DELAY =
+                    TransactionController.class.getDeclaredMethod("computeTransactionPacingDelay", long.class);
+            COMPUTE_TRANSACTION_PACING_DELAY.setAccessible(true);
+            RECORD_TRANSACTION_START =
+                    TransactionController.class.getDeclaredMethod("recordTransactionStart", long.class);
+            RECORD_TRANSACTION_START.setAccessible(true);
+            NEXT_PACING_START_TIME =
+                    TransactionController.class.getDeclaredField("nextPacingStartTime");
+            NEXT_PACING_START_TIME.setAccessible(true);
+        } catch (NoSuchMethodException e) {
+            throw new ExceptionInInitializerError(e);
+        } catch (NoSuchFieldException e) {
+            throw new ExceptionInInitializerError(e);
+        }
+    }
 
     @Test
     public void testIssue57958() throws Exception {
@@ -80,5 +118,168 @@ public class TestTransactionController extends JMeterTestCase {
                 "Must one transaction samples with parent debug sample");
         assertEquals("Number of samples in transaction : 1, number of failing samples : 1",
                 listener.getEvents().get(0).getResult().getResponseMessage());
+    }
+
+    @Test
+    public void testFixedDelay() throws Exception {
+        TransactionController controller = new TransactionController();
+        controller.setDelayMode(TransactionController.DELAY_FIXED);
+        controller.setFixedDelay("15");
+
+        assertEquals(15, computeTransactionDelay(controller));
+    }
+
+    @Test
+    public void testRandomDelaySupportsSameMinAndMax() throws Exception {
+        TransactionController controller = new TransactionController();
+        controller.setDelayMode(TransactionController.DELAY_RANDOM);
+        controller.setDelayMin("7");
+        controller.setDelayMax("7");
+
+        assertEquals(7, computeTransactionDelay(controller));
+    }
+
+    @Test
+    public void testGaussianRandomDelaySupportsSameMinAndMax() throws Exception {
+        TransactionController controller = new TransactionController();
+        controller.setDelayMode(TransactionController.DELAY_GAUSSIAN_RANDOM);
+        controller.setDelayMin("9");
+        controller.setDelayMax("9");
+
+        assertEquals(9, computeTransactionDelay(controller));
+    }
+
+    @Test
+    public void testRandomDelayRejectsInvalidRange() {
+        TransactionController controller = new TransactionController();
+        controller.setDelayMode(TransactionController.DELAY_RANDOM);
+        controller.setDelayMin("10");
+        controller.setDelayMax("5");
+
+        assertThrows(IllegalArgumentException.class, () -> computeTransactionDelay(controller));
+    }
+
+    @Test
+    public void testDelayValuesSupportVariables() throws Exception {
+        JMeterVariables variables = new JMeterVariables();
+        variables.put("delayMs", "12");
+        JMeterContextService.getContext().setVariables(variables);
+
+        TransactionController controller = new TransactionController();
+        controller.setDelayMode(TransactionController.DELAY_FIXED);
+        controller.setProperty(functionProperty(
+                TransactionControllerSchema.INSTANCE.getFixedDelay().getName(),
+                "${delayMs}"));
+
+        assertEquals(12, computeTransactionDelay(controller));
+    }
+
+    @Test
+    public void testPacingDoesNotDelayFirstIteration() throws Exception {
+        TransactionController controller = new TransactionController();
+        controller.setPacingMode(TransactionController.DELAY_FIXED);
+        controller.setFixedPacing("100");
+
+        assertEquals(0, computeTransactionPacingDelay(controller, 1000));
+    }
+
+    @Test
+    public void testFixedPacingUsesDiffFromPreviousTransactionStart() throws Exception {
+        TransactionController controller = new TransactionController();
+        controller.setPacingMode(TransactionController.DELAY_FIXED);
+        controller.setFixedPacing("100");
+        setNextPacingStartTime(controller, 1100);
+
+        assertEquals(60, computeTransactionPacingDelay(controller, 1040));
+    }
+
+    @Test
+    public void testPacingDoesNotDelayWhenCurrentStartExceededTarget() throws Exception {
+        TransactionController controller = new TransactionController();
+        controller.setPacingMode(TransactionController.DELAY_FIXED);
+        controller.setFixedPacing("100");
+        setNextPacingStartTime(controller, 1100);
+
+        assertEquals(0, computeTransactionPacingDelay(controller, 1120));
+    }
+
+    @Test
+    public void testRandomPacingSupportsSameMinAndMax() throws Exception {
+        TransactionController controller = new TransactionController();
+        controller.setPacingMode(TransactionController.DELAY_RANDOM);
+        controller.setPacingMin("70");
+        controller.setPacingMax("70");
+        setNextPacingStartTime(controller, 1070);
+
+        assertEquals(40, computeTransactionPacingDelay(controller, 1030));
+    }
+
+    @Test
+    public void testPacingValuesSupportVariables() throws Exception {
+        JMeterVariables variables = new JMeterVariables();
+        variables.put("pacingMs", "75");
+        JMeterContextService.getContext().setVariables(variables);
+
+        TransactionController controller = new TransactionController();
+        controller.setPacingMode(TransactionController.DELAY_FIXED);
+        controller.setProperty(functionProperty(
+                TransactionControllerSchema.INSTANCE.getFixedPacing().getName(),
+                "${pacingMs}"));
+        setNextPacingStartTime(controller, 1075);
+
+        assertEquals(50, computeTransactionPacingDelay(controller, 1025));
+    }
+
+    @Test
+    public void testPacingCompensatesForDrift() throws Exception {
+        TransactionController controller = new TransactionController();
+        controller.setPacingMode(TransactionController.DELAY_FIXED);
+        controller.setFixedPacing("100");
+        setNextPacingStartTime(controller, 1100);
+
+        recordTransactionStart(controller, 1103);
+
+        assertEquals(97, computeTransactionPacingDelay(controller, 1103));
+    }
+
+    private static long computeTransactionDelay(TransactionController controller) throws Exception {
+        return invokeLong(COMPUTE_TRANSACTION_DELAY, controller);
+    }
+
+    private static long computeTransactionPacingDelay(TransactionController controller, long now) throws Exception {
+        return invokeLong(COMPUTE_TRANSACTION_PACING_DELAY, controller, now);
+    }
+
+    private static long invokeLong(Method method, TransactionController controller, Object... args) throws Exception {
+        try {
+            return (Long) method.invoke(controller, args);
+        } catch (InvocationTargetException e) {
+            Throwable cause = e.getCause();
+            if (cause instanceof RuntimeException runtimeException) {
+                throw runtimeException;
+            }
+            if (cause instanceof Error error) {
+                throw error;
+            }
+            throw e;
+        }
+    }
+
+    private static void recordTransactionStart(TransactionController controller, long actualStartTime)
+            throws Exception {
+        RECORD_TRANSACTION_START.invoke(controller, actualStartTime);
+    }
+
+    private static void setNextPacingStartTime(TransactionController controller, long startTime)
+            throws Exception {
+        NEXT_PACING_START_TIME.set(controller, startTime);
+    }
+
+    private static JMeterProperty functionProperty(String propertyName, String value) throws Exception {
+        ReplaceStringWithFunctions transformer =
+                new ReplaceStringWithFunctions(new CompoundVariable(), new HashMap<>());
+        JMeterProperty property = transformer.transformValue(new StringProperty(propertyName, value));
+        property.setRunningVersion(true);
+        return property;
     }
 }
--- src/core/src/main/java/org/apache/jmeter/control/gui/TransactionControllerGui.java
+++ src/core/src/main/java/org/apache/jmeter/control/gui/TransactionControllerGui.java
@@ -17,6 +17,15 @@
 
 package org.apache.jmeter.control.gui;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 import org.apache.jmeter.control.TransactionController;
 import org.apache.jmeter.control.TransactionControllerSchema;
 import org.apache.jmeter.gui.GUIMenuSortOrder;
@@ -49,6 +58,50 @@ public class TransactionControllerGui extends AbstractControllerGui {
                     "transaction_controller_include_timers",
                     JMeterUtils::getResString);
 
+    private final JComboBox<String> delayMode = new JComboBox<>(new String[] {
+            TransactionController.DELAY_DISABLED,
+            TransactionController.DELAY_FIXED,
+            TransactionController.DELAY_RANDOM,
+            TransactionController.DELAY_GAUSSIAN_RANDOM,
+    });
+
+    private final JTextField fixedDelay = new JTextField(12);
+
+    private final JTextField delayMin = new JTextField(12);
+
+    private final JTextField delayMax = new JTextField(12);
+
+    private final JLabel fixedDelayLabel =
+            new JLabel(JMeterUtils.getResString("transaction_controller_delay_fixed"));
+
+    private final JLabel delayMinLabel =
+            new JLabel(JMeterUtils.getResString("transaction_controller_delay_min"));
+
+    private final JLabel delayMaxLabel =
+            new JLabel(JMeterUtils.getResString("transaction_controller_delay_max"));
+
+    private final JComboBox<String> pacingMode = new JComboBox<>(new String[] {
+            TransactionController.DELAY_DISABLED,
+            TransactionController.DELAY_FIXED,
+            TransactionController.DELAY_RANDOM,
+            TransactionController.DELAY_GAUSSIAN_RANDOM,
+    });
+
+    private final JTextField fixedPacing = new JTextField(12);
+
+    private final JTextField pacingMin = new JTextField(12);
+
+    private final JTextField pacingMax = new JTextField(12);
+
+    private final JLabel fixedPacingLabel =
+            new JLabel(JMeterUtils.getResString("transaction_controller_pacing_fixed"));
+
+    private final JLabel pacingMinLabel =
+            new JLabel(JMeterUtils.getResString("transaction_controller_pacing_min"));
+
+    private final JLabel pacingMaxLabel =
+            new JLabel(JMeterUtils.getResString("transaction_controller_pacing_max"));
+
     /**
      * Create a new TransactionControllerGui instance.
      */
@@ -64,10 +117,57 @@ public class TransactionControllerGui extends AbstractControllerGui {
     }
 
     @Override
+    public void configure(TestElement element) {
+        super.configure(element);
+        if (element instanceof TransactionController transactionController) {
+            delayMode.setSelectedItem(transactionController.getDelayMode());
+            fixedDelay.setText(transactionController.getFixedDelay());
+            delayMin.setText(transactionController.getDelayMin());
+            delayMax.setText(transactionController.getDelayMax());
+            pacingMode.setSelectedItem(transactionController.getPacingMode());
+            fixedPacing.setText(transactionController.getFixedPacing());
+            pacingMin.setText(transactionController.getPacingMin());
+            pacingMax.setText(transactionController.getPacingMax());
+            updateDelayFields();
+            updatePacingFields();
+        }
+    }
+
+    @Override
+    public void modifyTestElement(TestElement element) {
+        super.modifyTestElement(element);
+        if (element instanceof TransactionController transactionController) {
+            transactionController.setDelayMode((String) delayMode.getSelectedItem());
+            transactionController.setFixedDelay(fixedDelay.getText());
+            transactionController.setDelayMin(delayMin.getText());
+            transactionController.setDelayMax(delayMax.getText());
+            transactionController.setPacingMode((String) pacingMode.getSelectedItem());
+            transactionController.setFixedPacing(fixedPacing.getText());
+            transactionController.setPacingMin(pacingMin.getText());
+            transactionController.setPacingMax(pacingMax.getText());
+        }
+    }
+
+    @Override
     public void assignDefaultValues(TestElement element) {
         super.assignDefaultValues(element);
         // See https://github.com/apache/jmeter/issues/3282
         ((TransactionController) element).setIncludeTimers(false);
+    }
+
+    @Override
+    public void clearGui() {
+        super.clearGui();
+        delayMode.setSelectedItem(TransactionController.DELAY_DISABLED);
+        fixedDelay.setText("0"); // $NON-NLS-1$
+        delayMin.setText("0"); // $NON-NLS-1$
+        delayMax.setText("0"); // $NON-NLS-1$
+        pacingMode.setSelectedItem(TransactionController.DELAY_DISABLED);
+        fixedPacing.setText("0"); // $NON-NLS-1$
+        pacingMin.setText("0"); // $NON-NLS-1$
+        pacingMax.setText("0"); // $NON-NLS-1$
+        updateDelayFields();
+        updatePacingFields();
     }
 
     @Override
@@ -84,5 +184,131 @@ public class TransactionControllerGui extends AbstractControllerGui {
         add(makeTitlePanel());
         add(generateParentSample);
         add(includeTimers);
+        add(createDelayPanel());
+        add(createPacingPanel());
+    }
+
+    private JPanel createDelayPanel() {
+        JPanel delayPanel = new JPanel(new GridBagLayout());
+        delayPanel.setBorder(
+                javax.swing.BorderFactory.createTitledBorder(
+                        JMeterUtils.getResString("transaction_controller_delay")));
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.insets = new Insets(2, 2, 2, 2);
+        constraints.anchor = GridBagConstraints.WEST;
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        delayPanel.add(new JLabel(JMeterUtils.getResString("transaction_controller_delay_option")), constraints);
+        constraints.gridx = 1;
+        constraints.weightx = 1.0d;
+        delayPanel.add(delayMode, constraints);
+
+        constraints.gridy++;
+        constraints.gridx = 0;
+        constraints.weightx = 0;
+        delayPanel.add(fixedDelayLabel, constraints);
+        constraints.gridx = 1;
+        constraints.weightx = 1.0d;
+        delayPanel.add(fixedDelay, constraints);
+
+        constraints.gridy++;
+        constraints.gridx = 0;
+        constraints.weightx = 0;
+        delayPanel.add(delayMinLabel, constraints);
+        constraints.gridx = 1;
+        constraints.weightx = 1.0d;
+        delayPanel.add(delayMin, constraints);
+
+        constraints.gridy++;
+        constraints.gridx = 0;
+        constraints.weightx = 0;
+        delayPanel.add(delayMaxLabel, constraints);
+        constraints.gridx = 1;
+        constraints.weightx = 1.0d;
+        delayPanel.add(delayMax, constraints);
+
+        delayMode.addActionListener(e -> updateDelayFields());
+        updateDelayFields();
+        return delayPanel;
+    }
+
+    private void updateDelayFields() {
+        String mode = (String) delayMode.getSelectedItem();
+        boolean fixed = TransactionController.DELAY_FIXED.equals(mode);
+        boolean random = TransactionController.DELAY_RANDOM.equals(mode)
+                || TransactionController.DELAY_GAUSSIAN_RANDOM.equals(mode);
+
+        fixedDelayLabel.setVisible(fixed);
+        fixedDelay.setVisible(fixed);
+        delayMinLabel.setVisible(random);
+        delayMin.setVisible(random);
+        delayMaxLabel.setVisible(random);
+        delayMax.setVisible(random);
+        revalidate();
+        repaint();
+    }
+
+    private JPanel createPacingPanel() {
+        JPanel pacingPanel = new JPanel(new GridBagLayout());
+        pacingPanel.setBorder(
+                javax.swing.BorderFactory.createTitledBorder(
+                        JMeterUtils.getResString("transaction_controller_pacing")));
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.insets = new Insets(2, 2, 2, 2);
+        constraints.anchor = GridBagConstraints.WEST;
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        pacingPanel.add(new JLabel(JMeterUtils.getResString("transaction_controller_pacing_option")), constraints);
+        constraints.gridx = 1;
+        constraints.weightx = 1.0d;
+        pacingPanel.add(pacingMode, constraints);
+
+        constraints.gridy++;
+        constraints.gridx = 0;
+        constraints.weightx = 0;
+        pacingPanel.add(fixedPacingLabel, constraints);
+        constraints.gridx = 1;
+        constraints.weightx = 1.0d;
+        pacingPanel.add(fixedPacing, constraints);
+
+        constraints.gridy++;
+        constraints.gridx = 0;
+        constraints.weightx = 0;
+        pacingPanel.add(pacingMinLabel, constraints);
+        constraints.gridx = 1;
+        constraints.weightx = 1.0d;
+        pacingPanel.add(pacingMin, constraints);
+
+        constraints.gridy++;
+        constraints.gridx = 0;
+        constraints.weightx = 0;
+        pacingPanel.add(pacingMaxLabel, constraints);
+        constraints.gridx = 1;
+        constraints.weightx = 1.0d;
+        pacingPanel.add(pacingMax, constraints);
+
+        pacingMode.addActionListener(e -> updatePacingFields());
+        updatePacingFields();
+        return pacingPanel;
+    }
+
+    private void updatePacingFields() {
+        String mode = (String) pacingMode.getSelectedItem();
+        boolean fixed = TransactionController.DELAY_FIXED.equals(mode);
+        boolean random = TransactionController.DELAY_RANDOM.equals(mode)
+                || TransactionController.DELAY_GAUSSIAN_RANDOM.equals(mode);
+
+        fixedPacingLabel.setVisible(fixed);
+        fixedPacing.setVisible(fixed);
+        pacingMinLabel.setVisible(random);
+        pacingMin.setVisible(random);
+        pacingMaxLabel.setVisible(random);
+        pacingMax.setVisible(random);
+        revalidate();
+        repaint();
     }
 }
